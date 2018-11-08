7aa5c391090ad3aa8a2a1b32a2878aabbf519da6
Tobias Koppers <tobias.koppers@googlemail.com>
2018-11-08 19:13:01 +0100

    bugfix

--- src/runtime/renderChildren.js
+++ src/runtime/renderChildren.js
@@ -1,9 +1,10 @@
 import createText from "./createText";
+import toText from "./toText";
 
 export default (parentNode, children) => {
 	for (const child of children) {
 		parentNode.appendChild(
-			typeof child === "string" ? createText(child) : child
+			typeof child === "string" ? createText(toText(child)) : child
 		);
 	}
 };
